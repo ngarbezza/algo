1e42d5ee335a60d5de6c8e790ffbbc90e14ce5da
Nahuel Garbezza <n.garbezza@gmail.com>
2015-04-29 08:34:30 -0300

    actualizado p5 ej2 usando una priority queue

--- practica5/p5ej2.rb
+++ practica5/p5ej2.rb
@@ -1,32 +1,94 @@
 require_relative '../extensions/array'
 
-# TODO preguntar por complejidad en el peor caso
-
 def aulas_para_cursos(cursos)
-  aulas = []
+  aulas = PQ.new
+  cursos = ordenar_por_hora_de_inicio(cursos)
   cursos.each do |curso|
-    pude_asignar_curso = false
-    aulas.each do |aula|
-      if curso[2] <= aula[0]      # puede entrar antes que todos los cursos del aula
-        aula[2].add_first curso
-        aula[0] = curso[1]        # actualizar hora inicial del aula
-        pude_asignar_curso = true
-      elsif curso[1] >= aula[1]   # puede entrar después que todos los cursos del aula
-        aula[2].add_last curso
-        aula[1] = curso[2]        # actualizar hora final del aula
-        pude_asignar_curso = true
-      else
-        # nada que hacer, el curso no se puede asignar en este aula
-      end
-    end
-    unless pude_asignar_curso
-      # necesito una nueva aula y asigno al curso ahí
-      aulas << [curso[1], curso[2], [curso]]
-    end
+    aulas.find(
+        lambda { |elem| curso_es_compatible?(curso, elem) },
+        lambda { |elem| actualizar_aula(aulas, elem, curso) },
+        lambda { agregar_aula(aulas, curso) }
+    )
   end
   aulas
 end
 
+# Definiciones Auxiliares
+
+def curso_es_compatible?(curso, elem)
+  elem.priority <= curso[1]
+end
+
+def ordenar_por_hora_de_inicio(cursos)
+  cursos.sort { |c1, c2| c1[1] <=> c2[1] }
+end
+
+def actualizar_aula(aulas, elem, curso)
+  aulas.remove(elem)
+  aulas << PQElem.new(elem.obj + [curso], curso[2])
+end
+
+def agregar_aula(aulas, curso)
+  aulas << PQElem.new([curso], curso[2])
+end
+
+class PQElem
+  include Comparable
+
+  attr_accessor :obj, :priority
+
+  def initialize(obj, priority)
+    @obj, @priority = obj, priority
+  end
+
+  def <=>(other)
+    @priority <=> other.priority
+  end
+
+  def ==(other)
+    @priority == other.priority && @obj == other.obj
+  end
+end
+
+class PQ
+  # implementación sencilla de priority queue. Asume que
+  # conceptualmente todas sus operaciones son O(log n)
+
+  def initialize
+    @elements = []
+  end
+
+  def <<(element)
+    @elements << element
+  end
+
+  def pop
+    last_element_index = @elements.size - 1
+    @elements.sort!
+    @elements.delete_at(last_element_index)
+  end
+
+  def find(predicate, if_found, if_not_found)
+    for i in 0..@elements.size-1
+      return if_found.call(@elements[i]) if predicate.call(@elements[i])
+    end
+    if_not_found.call
+  end
+
+  def remove(element)
+    @elements.delete(element)
+  end
+
+  def to_s
+    out = ''
+    @elements.each do |element|
+      out += "#{element.priority}: #{element.obj}\n"
+    end
+    out
+  end
+end
+
+# Código de prueba
 
 cursos = [
     [:historia, 2, 5],
