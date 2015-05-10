aea6fb5c60ce77c818331ea820abcaf199c6c51f
Nahuel Garbezza <n.garbezza@gmail.com>
2015-05-10 13:37:46 -0300

    mejoras a p6 ej 14

--- practica6/p6ej14.rb
+++ practica6/p6ej14.rb
@@ -2,13 +2,19 @@ def cantidad_de_componentes_conexas(gl)
   vs = [false] * gl.length
   s = 0 # partimos de un vértice arbitrario
   cant = 0
-  while vs.any? { |v| !v } # haya nodos sin visitar
+  while vertices_sin_visitar?(vs)
     vs[s] = true
     marcar_alcanzables(gl, s, vs)
     cant += 1 # una componente más
-    s = vs.index(false) # próximo sin visitar aún
+    s = vs.index(false) # próximo sin visitar
   end
   cant
+end
+
+# Definiciones auxiliares
+
+def vertices_sin_visitar?(vs)
+  vs.any? { |v| !v }
 end
 
 def marcar_alcanzables(gl, s, vs)
@@ -19,6 +25,8 @@ def marcar_alcanzables(gl, s, vs)
   end
 end
 
-puts cantidad_de_componentes_conexas([[], [], []]) # 3
-puts cantidad_de_componentes_conexas([[1], [0], []]) # 2
-puts cantidad_de_componentes_conexas([[1,2], [0,2], [0,1]]) # 1
+# Código de prueba
+
+puts cantidad_de_componentes_conexas([[], [], []]) == 3
+puts cantidad_de_componentes_conexas([[1], [0], []]) == 2
+puts cantidad_de_componentes_conexas([[1,2], [0,2], [0,1]]) == 1
