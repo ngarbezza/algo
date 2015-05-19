541a0646bb9cf4092d57ecd3b250dc305c2819fb
Nahuel Garbezza <n.garbezza@gmail.com>
2015-05-19 16:09:47 -0300

    arreglos al algoritmo de dijkstra & algoritmo de floyd

--- practica7/p7ej7.rb
+++ practica7/p7ej7.rb
@@ -7,30 +7,44 @@ def dijkstra(g)
   until sin_procesar.empty?
 
     # TODO: mejorar utilizando colas de prioridad
-    min_v = nil
-    min_d = inf
+    vertice_con_distancia_minima = nil
+    distancia_minima = inf
     sin_procesar.each do |v|
-      if distancias[v] < min_d
-        min_d = distancias[v]
-        min_v = v
+      if distancias[v] < distancia_minima
+        distancia_minima = distancias[v]
+        vertice_con_distancia_minima = v
       end
     end
 
-    g[min_v].each_with_index do |u, i|
+    g[vertice_con_distancia_minima].each_with_index do |u, i|
       if u
-        nueva_distancia_a_u = distancias[min_v] + u
+        nueva_distancia_a_u = distancias[vertice_con_distancia_minima] + u
         if nueva_distancia_a_u < distancias[i]
           distancias[i] = nueva_distancia_a_u
-          procesados[i] = min_v
+          procesados[i] = vertice_con_distancia_minima
         end
       end
     end
 
-    sin_procesar.delete min_v
+    sin_procesar.delete vertice_con_distancia_minima
 
   end
 
   [procesados, distancias]
+end
+
+def floyd(g)
+  d = g.clone
+  (0..g.length-1).each do |i|
+    (0..g.length-1).each do |j|
+      (0..g.length-1).each do |k|
+        if d[i][k] + d[k][j] < d[i][j]
+          d[i][j] = d[i][k] + d[k][j]
+        end
+      end
+    end
+  end
+  d
 end
 
 # ejemplo ejercicio 6
@@ -48,7 +62,15 @@ end
 _ = nil
 puts dijkstra([
     [_, 1, 4, _],
-    [_, _, 2, _],
+    [_, _, 2, 9],
     [_, _, _, 1],
-    [_, _, _, 9]
+    [_, _, _, _]
+]).to_s
+
+x = Float::INFINITY
+puts floyd([
+    [0, 1, 4, x],
+    [x, 0, 2, 9],
+    [x, x, 0, 1],
+    [x, x, x, 0]
 ]).to_s
